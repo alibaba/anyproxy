770d495440daba56713f53ca024cd34a5075c693
加里 <xiaofeng.mxf@taobao.com>
2014-08-09 14:16:59 +0800

    add https server mode

--- index.js
+++ index.js
@@ -1,16 +1,50 @@
-var PROXY_PORT = 8001;
-
 var http      = require('http'),
     https     = require('https'),
     fs        = require('fs'),
     net       = require('net'),
     url       = require('url'),
     exec      = require('child_process').exec,
-    serverMgr = require("./lib/serverMgr");
+    serverMgr = require("./lib/serverMgr"),
+    createCert= require("./lib/createCert");
 
-var serverMgrInstance = new serverMgr();
+var PROXY_PORT    = 8001,
+    T_PROXY_HTTP  = 0,
+    T_PROXY_HTTPS = 1,
+    PROXY_TYPE    = T_PROXY_HTTPS,
+    HOSTNAME      = "localhost";
 
-var httpProxyServer = http.createServer(function (req, res) {
+var serverMgrInstance = new serverMgr(),
+    httpProxyServer;
+
+if(PROXY_TYPE == T_PROXY_HTTP){
+    httpProxyServer = http.createServer(dealProxyUserHttpReq);
+}else{
+
+    var keyFile = "./cert/tmpCert/__hostname.key".replace(/__hostname/,HOSTNAME),
+        crtFile = "./cert/tmpCert/__hostname.crt".replace(/__hostname/,HOSTNAME);
+
+    if(!fs.existsSync(keyFile) || !fs.existsSync(crtFile)){
+        createCert(HOSTNAME,function(){
+            httpProxyServer = https.createServer({
+                key : fs.readFileSync(keyFile),
+                cert: fs.readFileSync(crtFile)
+            },dealProxyUserHttpReq);
+        });
+    }else{
+        httpProxyServer = https.createServer({
+            key : fs.readFileSync(keyFile),
+            cert: fs.readFileSync(crtFile)
+        },dealProxyUserHttpReq);
+    }
+}
+
+//listen CONNECT method for https over http
+httpProxyServer.on('connect',dealProxyConnectReq);
+httpProxyServer.listen(PROXY_PORT);
+console.log( (PROXY_TYPE == T_PROXY_HTTP ? "Http" : "Https") + " proxy started at port " + PROXY_PORT);
+
+
+function dealProxyUserHttpReq(req,res){
     var urlPattern = url.parse(req.url);
     var options = {
         hostname : urlPattern.host,
@@ -20,6 +54,7 @@ var httpProxyServer = http.createServer(function (req, res) {
         headers  : req.headers
     };
 
+    //forward to real server
     var directReq = http.request(options,function(directRes){
         res.writeHead(directRes.statusCode , directRes.headers);
         directRes.pipe(res);
@@ -30,11 +65,11 @@ var httpProxyServer = http.createServer(function (req, res) {
         res.end();
     });
 
-    directReq.end();
-});
+    directReq.end();    
+}
 
-//connect method for HTTPS over http
-httpProxyServer.on('connect', function(req, socket, head){
+
+function dealProxyConnectReq(req, socket, head){
     var hostname = req.url.split(":")[0];
 
     //forward the https-request to local https server
@@ -55,7 +90,4 @@ httpProxyServer.on('connect', function(req, socket, head){
             console.log("err fetch HTTPS server for host:" + hostname);
         }
     });
-});
-
-httpProxyServer.listen(PROXY_PORT);
-console.log("proxy started at port " + PROXY_PORT);
+}
