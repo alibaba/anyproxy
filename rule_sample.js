784a9460dd633abdbe0d42e4d7f5357c22c182a8
OttoMao <ottomao@gmail.com>
2014-08-13 17:30:16 +0800

    update doc ,ready to push to npm

--- rule_sample.js
+++ rule_sample.js
@@ -1,10 +1,10 @@
 var rules = {
     "map" :[
         {
-            "host"      :/./,
-            "path"      :/\/path\/test/,
-            "localFile" :"",
-            "localDir"  :"~/"
+            "host"      :/./,            //regExp
+            "path"      :/\/path\/test/, //regExp
+            "localFile" :"",             //this file will be returned to user when host and path pattern both meets the request
+            "localDir"  :"~/"            //find the file of same name in localdir. anyproxy will not read localDir settings unless localFile is falsy
         }
         ,{
             "host"      :/./,
@@ -12,24 +12,10 @@ var rules = {
             "localFile" :"/Users/Stella/tmp/test.png",
             "localDir"  :"~/"
         }
-        ,{
-            "host"      :/./,
-            "path"      :/jpg/,
-            "localFile" :"/Users/Stella/tmp/test.png",
-            "localDir"  :"~/"
-        }
-        ,{
-            "host"      :/./,
-            "path"      :/gif/,
-            "localFile" :"/Users/Stella/tmp/test.png",
-            "localDir"  :"~/"
-        }
     ]
     ,"httpsConfig":{
-        // "bypassAll" : true,
-        // "interceptDomains":[/^.*alibaba-inc\.com$/]
-        "bypassAll" : false,
-        "interceptDomains":[]
+        "bypassAll" : false,  //by setting this to true, anyproxy will not intercept any https request
+        "interceptDomains":[/www\.a\.com/,/www\.b\.com/] //by setting bypassAll:false, requests towards these domains will be intercepted, and try to meet the map rules above
     }
 }
 
