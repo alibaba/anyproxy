a4c171e2b98d9e0d01c09c4aed9b69bf670424f0
加里 <xiaofeng.mxf@taobao.com>
2014-09-05 13:53:12 +0800

    update doc

--- rule_sample/rule__blank.js
+++ rule_sample/rule__blank.js
@@ -1,25 +1,51 @@
 module.exports = {
 	/*
-	these functions will overwrite the default ones, write your own when necessary.
+	These functions will overwrite the default ones, write your own when necessary.
+    Comments in Chinese are nothing but a translation of key points. Be relax if you dont understand.
+    致中文用户：中文注释都是只摘要，必要时请参阅英文注释。欢迎提出修改建议。
 	*/
     summary:function(){
         console.log("this is a blank rule for anyproxy");
     },
 
-	//whether to intercept this request by local logic
+
+
+
+
+
+    //=======================
+    //when getting a request from user
+    //收到用户请求之后
+    //=======================
+
+    //是否在本地直接发送响应（不再向服务器发出请求）
+	//whether to intercept this request by local logic 
 	//if the return value is true, anyproxy will call dealLocalResponse to get response data and will not send request to remote server anymore
 	shouldUseLocalResponse : function(req,reqBody){
         return false;
 	},
 
+    //如果shouldUseLocalResponse返回true，会调用这个函数来获取本地响应内容
     //you may deal the response locally instead of sending it to server
-    //this function be called when shouldUseLocalResponse returns true
-	//callback(statusCode,resHeader,responseData)
-	//e.g. callback(200,{"content-type":"text/html"},"hello world")
+    //this function be called when shouldUseLocalResponse returns true 
+    //callback(statusCode,resHeader,responseData)
+    //e.g. callback(200,{"content-type":"text/html"},"hello world")
 	dealLocalResponse : function(req,reqBody,callback){
         callback(statusCode,resHeader,responseData)
 	},
 
+
+
+
+
+
+
+    //=======================
+    //when ready to send a request to server
+    //向服务端发出请求之前
+    //=======================
+
+    //替换向服务器发出的请求协议（http和https的替换）
     //replace the request protocol when sending to the real server
     //protocol : "http" or "https"
     replaceRequestProtocol:function(req,protocol){
@@ -27,6 +53,7 @@ module.exports = {
     	return newProtocol;
     },
 
+    //替换向服务器发出的请求参数（option)
     //req is user's request which will be sent to the proxy server, docs : http://nodejs.org/api/http.html#http_http_request_options_callback
     //you may return a customized option to replace the original option
     //you should not write content-length header in options, since anyproxy will handle it for you
@@ -35,17 +62,29 @@ module.exports = {
         return newOption;
     },
 
+    //替换请求的body
     //replace the request body
     replaceRequestData: function(req,data){
         return data;
     },
 
+
+
+
+
+    //=======================
+    //when ready to send the response to user after receiving response from server
+    //向用户返回服务端的响应之前
+    //=======================
+
+    //替换服务器响应的http状态码
     //replace the statusCode before it's sent to the user
     replaceResponseStatusCode: function(req,res,statusCode){
     	var newStatusCode = statusCode;
     	return newStatusCode;
     },
 
+    //替换服务器响应的http头
     //replace the httpHeader before it's sent to the user
     //Here header == res.headers
     replaceResponseHeader: function(req,res,header){
@@ -53,6 +92,7 @@ module.exports = {
     	return newHeader;
     },
 
+    //替换服务器响应的数据
     //replace the response from the server before it's sent to the user
     //you may return either a Buffer or a string
     //serverResData is a Buffer, you may get its content by calling serverResData.toString()
@@ -60,12 +100,23 @@ module.exports = {
         return serverResData;
     },
 
+    //在请求返回给用户前的延迟时间
     //add a pause before sending response to user
     pauseBeforeSendingResponse : function(req,res){
     	var timeInMS = 1; //delay all requests for 1ms
     	return timeInMS; 
     },
 
+
+
+
+
+
+    //=======================
+    //https config
+    //=======================
+
+    //是否截获https请求
     //should intercept https request, or it will be forwarded to real server
     shouldInterceptHttpsReq :function(req){
         return false;
