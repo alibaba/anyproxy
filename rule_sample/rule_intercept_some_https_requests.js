179090382fee2e1dc23921c7b2ef0b7af135b213
加里 <xiaofeng.mxf@taobao.com>
2014-09-11 12:04:06 +0800

    recover sample rule

--- rule_sample/rule_intercept_some_https_requests.js
+++ rule_sample/rule_intercept_some_https_requests.js
@@ -15,6 +15,7 @@ module.exports = {
         //intercept https://github.com/
         //otherwise, all the https traffic will not go through this proxy
 
+        // return true;
         if(req.headers.host == "github.com"){
             return true;
         }else{
