7c4e4a040d1dfecb603b90ed76a3b6ec930b3d65
加里 <xiaofeng.mxf@taobao.com>
2014-12-08 15:35:46 +0800

    bugfix

--- proxy.js
+++ proxy.js
@@ -51,15 +51,20 @@ var T_TYPE_HTTP            = 0,
     DEFAULT_TYPE           = T_TYPE_HTTP;
 
 var default_rule = require('./lib/rule_default');
+//may be unreliable in windows
 var anyproxyHome = path.join(util.getUserHome(),"/.anyproxy/");
-if(!fs.existsSync(anyproxyHome)){
-    fs.mkdirSync(anyproxyHome);
-}
-if(fs.existsSync(path.join(anyproxyHome,"rule_default.js"))){
-    default_rule = require(path.join(anyproxyHome,"rule_default"));
-}
-if(fs.existsSync(path.join(process.cwd(),'rule.js'))){
-    default_rule = require(path.join(process.cwd(),'rule'));
+try{
+    if(!fs.existsSync(anyproxyHome)){
+        fs.mkdirSync(anyproxyHome);
+    }
+    if(fs.existsSync(path.join(anyproxyHome,"rule_default.js"))){
+        default_rule = require(path.join(anyproxyHome,"rule_default"));
+    }
+    if(fs.existsSync(path.join(process.cwd(),'rule.js'))){
+        default_rule = require(path.join(process.cwd(),'rule'));
+    }
+}catch(e){
+
 }
 
 //option
