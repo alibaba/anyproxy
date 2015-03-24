451e8f050db854b0f79a8839f24c3157b2f27c4d
想当当 <donghua.yan@alipay.com>
2015-03-24 22:44:53 +0800

    updates

---
+++ rule_sample/rule_task.js
@@ -0,0 +1,23 @@
+var isRootCAFileExists = require("./certMgr.js").isRootCAFileExists(),
+    interceptFlag = false;
+
+var replaceServerResDataAsync = AnyProxy.phaseManager.getPhase('replaceServerResDataAsync');
+replaceServerResDataAsync.addTask(function(req,res,serverResData,callback){
+    console.log('first task');
+    callback(serverResData);
+});
+replaceServerResDataAsync.addTask(function(req,res,serverResData,callback){
+    console.log('second task');
+    callback(serverResData);
+});
+replaceServerResDataAsync.addTask(function(req,res,serverResData,callback){
+    console.log('third task');
+    callback(serverResData);
+});
+
+
+module.exports = {
+
+    phaseManager:AnyProxy.phaseManager
+
+};
