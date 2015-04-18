0aa2189c4cbc9642382f2d222f46cc1444eeee32
想当当 <donghua.yan@alipay.com>
2015-04-19 00:38:29 +0800

    opensslConf

--- cert/gen-rootCA.cmd
+++ cert/gen-rootCA.cmd
@@ -1,9 +1,9 @@
 @echo off
-
+set opensslConf=%2
 openssl genrsa -out rootCA.key 2048
 openssl req -x509 -new -nodes -key rootCA.key -days 36500 -out rootCA.crt \
     -subj "/C=CN/ST=SH/L=SH/O=AnyProxy/OU=Section/CN=Anyproxy SSL Proxying/emailAddress=AnyProxy@AnyProxy" \
-    -config /usr/local/lib/node_modules/anyproxy/cert/openssl.cnf \
+    -config %opensslConf% \
     -extensions v3_ca
 echo =============
 echo rootCA generated at :
