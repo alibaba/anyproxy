4cffa6c304dad6c0a99327c9f7d10fbccce6a680
砚然 <yanran.wwj@alipay.com>
2016-11-03 17:23:24 +0800

    change the detail panel style to tab style

--- web2/src/component/record-detail.jsx
+++ web2/src/component/record-detail.jsx
@@ -7,7 +7,7 @@ import React, { PropTypes } from 'react';
 import ReactDOM from 'react-dom';
 import ClassBind from 'classnames/bind';
 import { connect } from 'react-redux';
-import { Input, Alert } from 'antd';
+import { Input, Alert, Menu } from 'antd';
 import JsonViewer from 'component/json-viewer';
 import ModalPanel from 'component/modal-panel';
 import { hideRecordDetail } from 'action/recordAction';
@@ -17,11 +17,20 @@ import Style from './record-detail.less';
 import CommonStyle from '../style/common.less';
 
 const StyleBind = ClassBind.bind(Style);
+const PageIndexMap = {
+    REQUEST_INDEX: 'REQUEST_INDEX',
+    RESPONSE_INDEX: 'RESPONSE_INDEX'
+};
 
 class RecordDetail extends React.Component {
     constructor () {
         super();
         this.onClose = this.onClose.bind(this);
+        this.state = {
+            pageIndex: PageIndexMap.REQUEST_INDEX
+        };
+
+        this.onMenuChange = this.onMenuChange.bind(this);
     }
 
     static propTypes = {
@@ -32,6 +41,12 @@ class RecordDetail extends React.Component {
 
     onClose () {
         this.props.dispatch(hideRecordDetail());
+    }
+
+    onMenuChange (e) {
+        this.setState({
+            pageIndex: e.key,
+        });
     }
 
     getLiDivs (targetObj) {
@@ -93,13 +108,7 @@ class RecordDetail extends React.Component {
         );
     }
 
-    getRecordDetailDiv () {
-        const recordDetail = this.props.requestRecord.recordDetail;
-        if (!recordDetail) {
-            return null;
-        }
-
-        const statusStyle = StyleBind({ 'okStatus': recordDetail.statusCode === 200 });
+    getRequestDiv (recordDetail) {
         const reqSummary = (
             <span>
                 <span>{recordDetail.method}</span>
@@ -108,18 +117,11 @@ class RecordDetail extends React.Component {
             </span>
         );
 
-        const resSummary =(
-            <span>
-                <span>HTTP/1.1 </span>
-                <span className={statusStyle} > {recordDetail.statusCode} </span>
-            </span>
-        );
-
         return (
-            <div className={Style.detailWrapper} >
+            <div>
                 <div className={Style.section} >
                     <div >
-                        <span className={CommonStyle.sectionTitle}>Request Header</span>
+                        <span className={CommonStyle.sectionTitle}>Header</span>
                     </div>
                     <div className={CommonStyle.whiteSpace10} />
                     <ul>
@@ -132,15 +134,31 @@ class RecordDetail extends React.Component {
 
                 <div className={Style.section} >
                     <div >
-                        <span className={CommonStyle.sectionTitle}>Request Body</span>
+                        <span className={CommonStyle.sectionTitle}>Body</span>
                     </div>
                     <div className={CommonStyle.whiteSpace10} />
                     {this.getReqBodyDiv()}
                 </div>
+            </div>
+        );
+    }
 
+    getResponseDiv (recordDetail) {
+
+        const statusStyle = StyleBind({ 'okStatus': recordDetail.statusCode === 200 });
+
+        const resSummary =(
+            <span>
+                <span>HTTP/1.1 </span>
+                <span className={statusStyle} > {recordDetail.statusCode} </span>
+            </span>
+        );
+
+        return (
+            <div>
                 <div className={Style.section} >
                     <div >
-                        <span className={CommonStyle.sectionTitle}>Response Header</span>
+                        <span className={CommonStyle.sectionTitle}>Header</span>
                     </div>
                     <div className={CommonStyle.whiteSpace10} />
                     <ul>
@@ -153,12 +171,32 @@ class RecordDetail extends React.Component {
 
                 <div className={Style.section} >
                     <div >
-                        <span className={CommonStyle.sectionTitle}>Response Body</span>
+                        <span className={CommonStyle.sectionTitle}>Body</span>
                     </div>
                     <div className={CommonStyle.whiteSpace10} />
                     {this.getResBodyDiv()}
                 </div>
+            </div>
+        );
+    }
 
+    getRecordDetailDiv () {
+        const recordDetail = this.props.requestRecord.recordDetail;
+        if (!recordDetail) {
+            return null;
+        }
+
+        const menuBody = this.state.pageIndex === PageIndexMap.REQUEST_INDEX ?
+            this.getRequestDiv(recordDetail) : this.getResponseDiv(recordDetail);
+        return (
+            <div className={Style.wrapper} >
+                <Menu onClick={this.onMenuChange} mode="horizontal" selectedKeys={[this.state.pageIndex]} >
+                    <Menu.Item key={PageIndexMap.REQUEST_INDEX}>Request</Menu.Item>
+                    <Menu.Item key={PageIndexMap.RESPONSE_INDEX}>Preview</Menu.Item>
+                </Menu>
+                <div className={Style.detailWrapper} >
+                    {menuBody}
+                </div>
             </div>
         );
     }
